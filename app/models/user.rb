baf159ba3733162b6ac2ecf49261698f99f6de1b
rudenate3 <nathan.odin@gmail.com>
2018-08-18 23:46:09 -0400

    mp-2 Configure Devise to use Token Auth
    -(feature) Configure Devise to use Token Auth
    
    Closes #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
 end
