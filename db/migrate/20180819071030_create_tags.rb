3ae36d27b2992393bb3325d7aca649011b39ab15
rudenate3 <nathan.odin@gmail.com>
2018-08-20 00:19:16 -0400

    mp-36 Fix migrations to not be null
    -(refactor) Refactor migrations to make certain fields not nullable
    -(refactor) Refactor seed file so 0 is the default for favorited
    
    Closes: #36

--- db/migrate/20180819071030_create_tags.rb
+++ db/migrate/20180819071030_create_tags.rb
@@ -1,8 +1,8 @@
 class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
-      t.string :title
-      t.string :description
+      t.string :title, null: false
+      t.string :description, null: false
 
       t.timestamps
     end
