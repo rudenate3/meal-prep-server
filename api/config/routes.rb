e261c0581a9b3d65cee1edc525dc557296842f9c
rudenate3 <nathan.odin@gmail.com>
2020-03-22 21:33:41 -0400

    Add JWT authentication

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
   end
   post "/graphql", to: "graphql#execute"
+  resources :users, param: :_username
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_201502) do
+ActiveRecord::Schema.define(version: 2020_03_23_010135) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
